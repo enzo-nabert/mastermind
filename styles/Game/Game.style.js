ab4cb2acc4c10562f7a6546fa894520825f4c1f7
enzo-nabert <enzo.nabert@etu.umontpellier.fr>
2021-12-07 12:19:23 +0100

    fix: style

--- styles/Game/Game.style.js
+++ styles/Game/Game.style.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 
 export const GameSection = styled.section`
+    padding-top: 60px;
     height: 100vh;
     width: 100vw;
 
     display: flex;
     flex-direction: column;
-    justify-content: center;
     align-items: center;
 `;
 
